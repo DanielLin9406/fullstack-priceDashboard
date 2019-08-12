8576912903565d7bff22138276947ca567c02938
Daniel Lin <daniel.lin@positivegrid.com>
2019-08-12 19:03:47 +0800

    Add build image script

--- start-dev.sh
+++ start-dev.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
+FullPath=$PWD
+declare -a Containers=('nginx' 'prices' 'promotions' 'upgrade-rules' 'user' 'web')
+declare -a ServicesName=(["nginx"]="$FullPath/nginx" ["prices"]="$FullPath/services/prices" ["promotions"]="$FullPath/services/promotions" ["upgrade-rules"]="$FullPath/services/upgrade-rules" ["user"]="$FullPath/services/user" ["web"]="$FullPath/services/web")
 
-# Build image for a particular services 
-docker build -t pg-price-dashboard_nginx:latest -f ./docker-dev.dockerfile .
-docker build -t pg-price-dashboard_web:latest -f ./docker-dev.dockerfile .
-docker build -t pg-price-dashboard_user:latest -f ./docker-dev.dockerfile .
-docker build -t pg-price-dashboard_prices:latest -f ./docker-dev.dockerfile .
-docker build -t pg-price-dashboard_promotions:latest -f ./docker-dev.dockerfile .
-docker build -t pg-price-dashboard_upgrade-rules:latest -f ./docker-dev.dockerfile .
+FolderName=${PWD##*/}
 
-# Build all image and run container
-docker-compose -f docker-compose-dev.yml --build
-docker-compose -f docker-compose-dev.yml up
-docker-compose up -d
+go2Folder(){
+  cd $(echo $1 | tr -d '\r')
+}
+
+for i in "${Containers[@]}"; do
+  # echo $i
+  # echo ${ServicesName[$i]}
+  [ ! "$(docker ps -a | grep $i)" ] && go2Folder ${ServicesName[$i]}
+done
+# # Build image for a particular services 
+# docker build -t pg-price-dashboard_nginx:latest -f ./docker-dev.dockerfile .
+# docker build -t pg-price-dashboard_web:latest -f ./docker-dev.dockerfile .
+# docker build -t pg-price-dashboard_user:latest -f ./docker-dev.dockerfile .
+# docker build -t pg-price-dashboard_prices:latest -f ./docker-dev.dockerfile .
+# docker build -t pg-price-dashboard_promotions:latest -f ./docker-dev.dockerfile .
+# docker build -t pg-price-dashboard_upgrade-rules:latest -f ./docker-dev.dockerfile .
+
+# # Build all image and run container
+# docker-compose -f docker-compose-dev.yml --build
+# docker-compose -f docker-compose-dev.yml up
+# docker-compose up -d
 
 
