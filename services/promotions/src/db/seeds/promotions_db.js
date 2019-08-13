cf3438b39c125e19f9fd06766df0f89363320f7a
Daniel Lin <daniel.lin@positivegrid.com>
2019-08-13 15:59:12 +0800

    update

--- services/promotions/src/db/seeds/promotions_db.js
+++ services/promotions/src/db/seeds/promotions_db.js
@@ -1,6 +1,6 @@
 export default [
   {
-    name: 'Black Friday Promotion',
+    name: 'White Friday Promotion',
     start_date: '2019-07-16',
     end_date: '2020-08-16',
     onLive: '0',
@@ -9,7 +9,7 @@ export default [
         sku: 'L2001',
         sale_price: 137,
         price: 1337,
-        name: 'BIAS AMP Eltie'
+        name: 'Name of L2001'
       }
     ]
   }
--- start-dev.sh
+++ start-dev.sh
@@ -7,8 +7,9 @@ declare -a Containers=('nginx' 'prices' 'promotions' 'upgrade-rules' 'user' 'web
 declare -A ServicesPath=( ["nginx"]="/nginx" ["prices"]="/services/prices" ["promotions"]="/services/promotions" ["upgrade-rules"]="/services/upgrade-rules" ["user"]="/services/user" ["web"]="/services/web" )
 
 dockerBuildImage(){
-  ServicesName=$1
-  docker build -t pg-price-dashboard_${ServicesName}:latest -f ./docker-dev.dockerfile .
+  RelativePath=$1
+  ServicesName=$2
+  docker build -t pg-price-dashboard_${ServicesName}:latest -f .${RelativePath}/docker-dev.dockerfile .${RelativePath}
 }
 
 dockerRemoveImage(){
@@ -16,25 +17,18 @@ dockerRemoveImage(){
   docker rmi $(echo ${FolderName}_${ServicesName})
 }
 
-go2Folder(){
-  RelativePath=$1
-  ServicesName=$2
-  cd $(echo ${FullPath}${RelativePath} | tr -d '\r')
-  dockerBuildImage $2
-}
-
 # Serach a need-to-be-updated container 
 for i in "${Containers[@]}"; do
-  [ ! "$(docker ps -a | grep $i)" ] && dockerRemoveImage $i && go2Folder ${ServicesPath[$i]} $i
+  [ ! "$(docker ps -a | grep $i)" ] && dockerRemoveImage $i && dockerBuildImage ${ServicesPath[$i]} $i
 done
 
 # Build image for a particular services 
-# docker build -t pg-price-dashboard_nginx:latest -f ./docker-dev.dockerfile .
-# docker build -t pg-price-dashboard_web:latest -f ./docker-dev.dockerfile .
-# docker build -t pg-price-dashboard_user:latest -f ./docker-dev.dockerfile .
-# docker build -t pg-price-dashboard_prices:latest -f ./docker-dev.dockerfile .
-# docker build -t pg-price-dashboard_promotions:latest -f ./docker-dev.dockerfile .
-# docker build -t pg-price-dashboard_upgrade-rules:latest -f ./docker-dev.dockerfile .
+# docker build -t pg-price-dashboard_nginx:latest -f ./nginx/docker-dev.dockerfile ./nginx
+# docker build -t pg-price-dashboard_web:latest -f ./services/web/docker-dev.dockerfile ./services/web
+# docker build -t pg-price-dashboard_user:latest -f ./services/user/docker-dev.dockerfile ./services/user
+# docker build -t pg-price-dashboard_prices:latest -f ./services/prices/docker-dev.dockerfile ./services/prices
+# docker build -t pg-price-dashboard_promotions:latest -f ./services/promotions/docker-dev.dockerfile ./services/promotions
+# docker build -t pg-price-dashboard_upgrade-rules:latest -f ./services/upgrade-rules/docker-dev.dockerfile ./services/upgrade-rules
 
 # # Build all image and run container
 # docker-compose -f docker-compose-dev.yml --build
